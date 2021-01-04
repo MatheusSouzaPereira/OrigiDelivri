30c5fa1fe57eb18bd5e12ed84f14fd33fe959a08
Matheus Souza Pereira <matheussouzap.dasilva@gmail.com>
2021-01-04 19:42:45 -0300

    CHORE: Project Create

---
+++ OrigiDelivry-BackEnd/src/test/java/com/origi/OrigiDelivry/OrigiDelivryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origi.OrigiDelivry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrigiDelivryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

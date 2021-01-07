03505e000c5cd7568c7e932b1e1ee91058035c09
Matheus Souza Pereira <matheussouzap.dasilva@gmail.com>
2021-01-07 00:35:57 -0300

    CHORE: POST/ORDES

--- OrigiDelivry-BackEnd/src/main/java/com/origi/OrigiDelivry/controllers/OrderController.java
+++ OrigiDelivry-BackEnd/src/main/java/com/origi/OrigiDelivry/controllers/OrderController.java
@@ -1,15 +1,20 @@
 package com.origi.OrigiDelivry.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.origi.OrigiDelivry.dto.OrderDTO;
 import com.origi.OrigiDelivry.services.OrderService;
+
 
 // Formas de fazer um Controlador REST no SpringBoot
 
@@ -29,5 +34,12 @@ public class OrderController {
 		return ResponseEntity.ok().body(list) ; 
 	}
 	
+	@PostMapping
+	public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO dto) {
+		dto = service.insert(dto) ; 
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri(); 
+		return ResponseEntity.created(uri).body(dto) ; 
+	}
 	
 }
--- OrigiDelivry-BackEnd/src/main/java/com/origi/OrigiDelivry/services/OrderService.java
+++ OrigiDelivry-BackEnd/src/main/java/com/origi/OrigiDelivry/services/OrderService.java
@@ -1,6 +1,7 @@
 package com.origi.OrigiDelivry.services;
 
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,12 +10,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.origi.OrigiDelivry.dto.OrderDTO;
+import com.origi.OrigiDelivry.dto.ProductDTO;
 import com.origi.OrigiDelivry.entities.Order;
+import com.origi.OrigiDelivry.entities.OrderStatus;
+import com.origi.OrigiDelivry.entities.Product;
 import com.origi.OrigiDelivry.repositories.OrderRepository;
-
-
-
-
+import com.origi.OrigiDelivry.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -23,10 +24,25 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository ; 
 	
+	@Autowired
+	private ProductRepository productRepository ; 
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll(){
 		List<Order> list = repository.findOrdersWithProducts(); 
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList()) ; 
 	}
 
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude() , dto.getLongitude(), 
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId()); 
+		order.getProducts().add(product);
+		}
+		order = repository.save(order) ; 
+		return new OrderDTO(order) ; 
+		
+	}
 }
